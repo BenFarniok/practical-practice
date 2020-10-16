bd985be6a707fc5f9b99a0db4844236decb8688d
unknown <benfarniok@gmail.com>
2020-10-16 15:36:01 -0500

    uploading basic practice docs

---
+++ basic_queries.sql
@@ -0,0 +1,21 @@
+SELECT * FROM movies;
+
+-- Find the title of each film ✓
+SELECT Title FROM movies;
+
+-- Find the director of each film ✓
+SELECT Director FROM movies:
+
+-- Find the title and director of each film ✓
+SELECT Title, Director FROM movies;
+
+-- Find the title and year of each film ✓
+SELECT Title, Year FROM movies; 
+
+-- Find all the information about each film 
+Select * FROM movies; 
+
+-- Practice sourced from sqlbolt.com
+
+
+
